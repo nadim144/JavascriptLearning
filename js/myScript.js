b638bed591e012a8fb0f5565f2b32f06da3591da
Nadim Ahmad <nadim144@users.noreply.github.com>
2020-04-14 16:35:40 +0530

    Chapter 3 and 5

---
+++ js/myScript.js
@@ -0,0 +1,6 @@
+document.write("External Javascript Example ...")
+
+
+//Add HTML <br> tag inside javascript.Similer way we can add many other tag inseide javascript.
+document.write("Hello Nadim <br> Hope you are doing fine.")
+document.write("<b><i> Whare are you now a days.?</i></b>")
